e8fd4b8af537b50d61c408128708ef1e24d94826
gentom <gento.m.developer@gmail.com>
2017-12-31 14:49:38 +0900

    add templates & commit to go stuff

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.LoadHTMLGlob("templates/*.tmpl")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(200, "index.tmpl", gin.H{
+			"hello": "Hi Gin-Gonic",
+		})
+	})
+
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
